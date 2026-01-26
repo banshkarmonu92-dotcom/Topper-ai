e31ec6c0c1ef629b31827aad4836929d50e7af22
banshkarmonu92-dotcom <banshkarmonu92@gmail.com>
2026-01-26 17:32:58 +0530

    Add subjects.h header file with explanation functions

---
+++ subjects.h
@@ -0,0 +1,9 @@
+#ifndef SUBJECTS_H
+#define SUBJECTS_H
+
+void ExplainMaths(char *response);
+void ExplainScience(char *response);
+void ExplainComputer(char *response);
+void ExplainAgain(int topic, char *response);
+
+#endif
